1a45ef72076dbbb02539a36b3277859b1033f142
MamangolivE <zulfikararyawinata650@gmail.com>
2026-03-01 11:48:20 +0800

    inputan

--- POSTTEST_1/POSTTEST_1/Form1.Designer.vb
+++ POSTTEST_1/POSTTEST_1/Form1.Designer.vb
@@ -22,10 +22,116 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        lblIPK = New Label()
+        nilaiIPK = New Label()
+        txtIP = New TextBox()
+        btnTambah = New Button()
+        lblPredikat = New Label()
+        Label1 = New Label()
+        btnReset = New Button()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' lblIPK
+        ' 
+        lblIPK.AutoSize = True
+        lblIPK.Location = New Point(351, 149)
+        lblIPK.Name = "lblIPK"
+        lblIPK.Size = New Size(41, 20)
+        lblIPK.TabIndex = 0
+        lblIPK.Text = "IPK : "
+        ' 
+        ' nilaiIPK
+        ' 
+        nilaiIPK.AutoSize = True
+        nilaiIPK.Location = New Point(411, 149)
+        nilaiIPK.Name = "nilaiIPK"
+        nilaiIPK.Size = New Size(17, 20)
+        nilaiIPK.TabIndex = 1
+        nilaiIPK.Text = "0"
+        ' 
+        ' txtIP
+        ' 
+        txtIP.Location = New Point(430, 218)
+        txtIP.Name = "txtIP"
+        txtIP.Size = New Size(60, 27)
+        txtIP.TabIndex = 2
+        txtIP.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(411, 262)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 3
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(411, 186)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(15, 20)
+        lblPredikat.TabIndex = 4
+        lblPredikat.Text = "-"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(318, 186)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(74, 20)
+        Label1.TabIndex = 5
+        Label1.Text = "Predikat : "
+        ' 
+        ' btnReset
+        ' 
+        btnReset.BackColor = Color.Red
+        btnReset.Location = New Point(298, 262)
+        btnReset.Margin = New Padding(0)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 6
+        btnReset.Text = "Reset"
+        btnReset.UseCompatibleTextRendering = True
+        btnReset.UseVisualStyleBackColor = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(318, 221)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(97, 20)
+        Label2.TabIndex = 7
+        Label2.Text = "IP Semester : "
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(btnReset)
+        Controls.Add(Label1)
+        Controls.Add(lblPredikat)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIP)
+        Controls.Add(nilaiIPK)
+        Controls.Add(lblIPK)
+        Name = "Form1"
         Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents lblIPK As Label
+    Friend WithEvents nilaiIPK As Label
+    Friend WithEvents txtIP As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents lblPredikat As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Label2 As Label
 
 End Class
--- POSTTEST_1/POSTTEST_1/Form1.vb
+++ POSTTEST_1/POSTTEST_1/Form1.vb
@@ -1,3 +1,35 @@
 ﻿Public Class Form1
+    Dim totalIPK As Double = 0
+    Dim jumlahSemester As Integer = 0
 
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim ips As Double
+        If Not Double.TryParse(txtIP.Text, ips) Then
+            MessageBox.Show("Input Harus Angka")
+            Exit Sub
+        End If
+
+        If ips < 0 Or ips > 4 Then
+            MessageBox.Show("IP Tidak Valid")
+            Exit Sub
+        End If
+
+        totalIPK += ips
+        jumlahSemester += 1
+
+        Dim ipk As Double = totalIPK / jumlahSemester
+        nilaiIPK.Text = ipk.ToString("0.00")
+
+        If ipk >= 2.0 And ipk <= 2.75 Then
+            lblPredikat.Text = "Cukup"
+        ElseIf ipk > 2.75 And ipk <= 3.0 Then
+            lblPredikat.Text = "Memuaskan"
+        ElseIf ipk > 3.0 Then
+            lblPredikat.Text = "Sangat Memuaskan"
+        Else
+            lblPredikat.Text = "-"
+        End If
+
+
+    End Sub
 End Class
